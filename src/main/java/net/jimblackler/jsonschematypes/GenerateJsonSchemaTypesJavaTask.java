debdfe5017be7ca5c1419383d1be091544729649
Jim Blackler <jimblackler@google.com>
2020-08-15 10:14:45 +0100

    Working plugin.

--- src/main/java/net/jimblackler/jsonschematypes/GenerateJsonSchemaTypesJavaTask.java
+++ src/main/java/net/jimblackler/jsonschematypes/GenerateJsonSchemaTypesJavaTask.java
@@ -3,9 +3,28 @@ package net.jimblackler.jsonschematypes;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
 public class GenerateJsonSchemaTypesJavaTask extends DefaultTask {
   @TaskAction
-  public void action() {
+  public void generate() throws IOException {
     System.out.println("In the task action");
+    JsonSchemaTypesPluginExtension extension =
+        getProject().getExtensions().getByType(JsonSchemaTypesPluginExtension.class);
+    System.out.println("Hello, " + extension.getGreeter());
+    System.out.println("I have a message for You: " + extension.getMessage());
+
+    Path outPath =
+        getProject().getBuildDir().toPath().resolve("generated").resolve("sources").resolve("jst");
+
+    Files.createDirectory(outPath);
+    Path exampleFile = outPath.resolve("test.txt");
+    List<String> lines = Arrays.asList("The first line", "The second line");
+    Files.write(exampleFile, lines, StandardCharsets.UTF_8);
   }
 }
--- src/main/java/net/jimblackler/jsonschematypes/JsonSchemaTypesPlugin.java
+++ src/main/java/net/jimblackler/jsonschematypes/JsonSchemaTypesPlugin.java
@@ -2,20 +2,33 @@ package net.jimblackler.jsonschematypes;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.file.SourceDirectorySet;
+import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.SourceSetContainer;
+
+import java.io.File;
 
 public class JsonSchemaTypesPlugin implements Plugin<Project> {
   @Override
   public void apply(Project project) {
-    JsonSchemaTypesPluginExtension extension =
-        project.getExtensions().create("jsonSchemaTypes", JsonSchemaTypesPluginExtension.class);
-    project.afterEvaluate(
-        project1 -> project1.getTasks().create("generateJsonSchemaTypes",
-            GenerateJsonSchemaTypesJavaTask.class));
 
-    project.task("hello").doLast(task -> {
-          System.out.println("Hello, " + extension.getGreeter());
-          System.out.println("I have a message for You: " + extension.getMessage());
-        }
-    );
+    project.getExtensions().create("jsonSchemaTypes", JsonSchemaTypesPluginExtension.class);
+    project.afterEvaluate(
+        project1 -> {
+          GenerateJsonSchemaTypesJavaTask task = project1.getTasks().create("generateJsonSchemaTypes",
+              GenerateJsonSchemaTypesJavaTask.class);
+          task.setGroup("build");
+          task.dependsOn(project1.getTasks().getByName("processResources"));  // needed?
+          project1.getTasks().getByName("compileJava").dependsOn(task);  // needed?
+
+          if (false) {
+            SourceSetContainer sourceSets =
+                (SourceSetContainer) project1.getProperties().get("sourceSets");
+            SourceSet main = sourceSets.getByName("main");
+            SourceDirectorySet java = main.getJava();
+            File buildDir1 = project1.getBuildDir();
+            java.srcDir(buildDir1.getAbsolutePath());
+          }
+        });
   }
 }
