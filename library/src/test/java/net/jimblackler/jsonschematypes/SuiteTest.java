18f41d5000681d9de40d5150eeddb9c68735b516
Jim Blackler <jimblackler@google.com>
2020-08-19 13:33:47 +0100

    Fully structured tests.

--- library/src/test/java/net/jimblackler/jsonschematypes/SuiteTest.java
+++ library/src/test/java/net/jimblackler/jsonschematypes/SuiteTest.java
@@ -32,112 +32,104 @@ public class SuiteTest {
   }
 
   @TestFactory
-  Collection<DynamicContainer> own() {
-    return scan(Path.of("/suites").resolve("own"), "http://json-schema.org/draft-07/schema#");
+  Collection<DynamicNode> tests() {
+    Path suites = Path.of("/suites");
+    return List.of(scan(suites.resolve("own"), "http://json-schema.org/draft-07/schema#"),
+        scan(suites.resolve("draft7"), "http://json-schema.org/draft-07/schema#"));
   }
 
-  @TestFactory
-  Collection<DynamicContainer> draft7() {
-    return scan(Path.of("/suites").resolve("draft7"), "http://json-schema.org/draft-07/schema#");
-  }
-
-  private static Collection<DynamicContainer> scan(Path testDir, String version) {
-    List<DynamicNode> everit = new ArrayList<>();
-    List<DynamicNode> own = new ArrayList<>();
-
-    System.out.println();
-    System.out.println(testDir);
-    System.out.println();
-
-    Collection<DynamicTest> tests = new ArrayList<>();
+  private static DynamicNode scan(Path testDir, String version) {
+    Collection<DynamicNode> allFileTests = new ArrayList<>();
     try (InputStream inputStream = ExampleTest.class.getResourceAsStream(testDir.toString());
          BufferedReader bufferedReader =
              new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
       String resource;
-
       while ((resource = bufferedReader.readLine()) != null) {
         if (resource.endsWith(".json")) {
-          System.out.println(resource);
-          JSONArray data = (JSONArray) Utils.getJsonObject(testDir.resolve(resource).toString());
-          for (int idx = 0; idx != data.length(); idx++) {
-            JSONObject testSet = data.getJSONObject(idx);
-            if (testSet.has("schema")) {
-              System.out.println(testSet.getString("description"));
-              Object schema = testSet.get("schema");
-              if (schema instanceof JSONObject) {
-                JSONObject schema1 = (JSONObject) schema;
-                schema1.put("$schema", version);
-
-                JSONArray tests1 = testSet.getJSONArray("tests");
-                List<DynamicTest> subTests = new ArrayList<>();
-
-                for (int idx2 = 0; idx2 != tests1.length(); idx2++) {
-                  JSONObject test = tests1.getJSONObject(idx2);
-                  subTests.add(dynamicTest(test.getString("description"), () -> {
-                    System.out.println("Schema:");
-                    System.out.println(schema1.toString(2));
-                    System.out.println();
-
-                    Schema everitSchema = SchemaLoader.load(schema1);
-
-                    System.out.println("Test:");
-                    System.out.println(test.toString(2));
-                    System.out.println();
-
-                    Object jsonObject = test.get("data");
-                    List<String> failures = null;
-                    try {
-                      everitSchema.validate(jsonObject);
-                    } catch (ValidationException ex) {
-                      System.out.println(ex.toJSON().toString());
-                      failures = ex.getAllMessages();
-                    } catch (Exception e) {
-                      fail(e);
-                    }
-                    boolean valid = test.getBoolean("valid");
-
-                    System.out.println("Schema:");
-                    System.out.println(schema1.toString(2));
-                    System.out.println();
-
-                    System.out.println("Test:");
-                    System.out.println(test.toString(2));
-                    System.out.println();
-
-                    if (failures != null) {
-                      System.out.println("Failures:");
-                      for (String message : failures) {
-                        System.out.println(message);
-                      }
-                      System.out.println();
-                    }
-
-                    System.out.print("Expeced to " + (valid ? "pass" : "fail") + " ... ");
-                    System.out.println((failures == null ? "passed" : "failed"));
-
-                    assertEquals(failures == null, valid);
-                  }));
-                }
-                everit.add(dynamicContainer(testSet.getString("description"), subTests));
-              }
-
-              own.add(dynamicTest(testSet.getString("description"), () -> {
-                SchemaStore schemaStore = new SchemaStore();
-                schemaStore.loadBaseObject(schema);
-                schemaStore.process();
-                System.out.println();
-              }));
-            }
-          }
-          System.out.println();
+          allFileTests.add(jsonTestFile(testDir, resource, version));
         }
       }
     } catch (IOException | GenerationException e) {
       throw new IllegalStateException(e);
     }
-    JSONObject outSchema = new JSONObject();
-    System.out.println(outSchema.toString(2));
+    return dynamicContainer(testDir.toString(), allFileTests);
+  }
 
-    return List.of(dynamicContainer("everit", everit), dynamicContainer("own", own));
+  private static DynamicNode jsonTestFile(Path testDir, String resource, String version)
+      throws GenerationException {
+    Collection<DynamicNode> nodes = new ArrayList<>();
+    JSONArray data = (JSONArray) Utils.getJsonObject(testDir.resolve(resource).toString());
+    for (int idx = 0; idx != data.length(); idx++) {
+      JSONObject testSet = data.getJSONObject(idx);
+      if (!testSet.has("schema")) {
+        continue; // ever happens?
+      }
+      nodes.add(singleSchemaTestList(testSet, version));
+    }
+    return dynamicContainer(resource, nodes);
+  }
+
+  private static DynamicNode singleSchemaTestList(JSONObject testSet, String version) {
+    Collection<DynamicTest> everitTests = new ArrayList<>();
+    Object schema = testSet.get("schema");
+    if (schema instanceof JSONObject) {
+      JSONObject schema1 = (JSONObject) schema;
+      schema1.put("$schema", version);
+      JSONArray tests1 = testSet.getJSONArray("tests");
+      for (int idx2 = 0; idx2 != tests1.length(); idx2++) {
+        JSONObject test = tests1.getJSONObject(idx2);
+        everitTests.add(dynamicTest(test.optString("description", "No description"), () -> {
+          System.out.println("Schema:");
+          System.out.println(schema1.toString(2));
+          System.out.println();
+
+          Schema everitSchema = SchemaLoader.load(schema1);
+
+          System.out.println("Test:");
+          System.out.println(test.toString(2));
+          System.out.println();
+
+          Object jsonObject = test.get("data");
+          List<String> failures = null;
+          try {
+            everitSchema.validate(jsonObject);
+          } catch (ValidationException ex) {
+            System.out.println(ex.toJSON().toString());
+            failures = ex.getAllMessages();
+          } catch (Exception e) {
+            fail(e);
+          }
+          boolean valid = test.getBoolean("valid");
+
+          System.out.println("Schema:");
+          System.out.println(schema1.toString(2));
+          System.out.println();
+
+          System.out.println("Test:");
+          System.out.println(test.toString(2));
+          System.out.println();
+
+          if (failures != null) {
+            System.out.println("Failures:");
+            for (String message : failures) {
+              System.out.println(message);
+            }
+            System.out.println();
+          }
+
+          System.out.print("Expeced to " + (valid ? "pass" : "fail") + " ... ");
+          System.out.println((failures == null ? "passed" : "failed"));
+
+          assertEquals(failures == null, valid);
+        }));
+      }
+    }
+    return dynamicContainer(testSet.getString("description"),
+        List.of(dynamicContainer("everit", everitTests), dynamicTest("own", () -> {
+          SchemaStore schemaStore = new SchemaStore();
+          schemaStore.loadBaseObject(schema);
+          schemaStore.process();
+          System.out.println();
+        })));
   }
 }
