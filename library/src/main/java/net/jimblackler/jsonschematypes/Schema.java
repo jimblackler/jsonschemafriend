651741d19b561d3b01a3b5bf5244e2c79ae30118
Jim Blackler <jimblackler@google.com>
2020-08-21 15:28:17 +0100

    Debugging assistance.

--- library/src/main/java/net/jimblackler/jsonschematypes/Schema.java
+++ library/src/main/java/net/jimblackler/jsonschematypes/Schema.java
@@ -4,9 +4,23 @@ import java.net.URI;
 import java.util.function.Consumer;
 
 public abstract class Schema {
+
+  private final URI path;
+
   Schema(SchemaStore schemaStore, URI path) throws GenerationException {
     // The schema is registered here to allow for circular graphs to be built in the constructors.
+    this.path = path;
     schemaStore.register(path, this);
   }
+
+  URI getPath() {
+    return path;
+  }
+
+  @Override
+  public String toString() {
+    return path.toString();
+  }
+
   abstract void validate(Object object, Consumer<ValidationError> errorConsumer);
 }
