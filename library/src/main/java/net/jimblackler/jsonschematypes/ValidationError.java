189db53b8c1df66b1feb2dba4d4199f459dfff71
Jim Blackler <jimblackler@google.com>
2020-08-26 18:47:41 +0100

    Safer error reporting.

--- library/src/main/java/net/jimblackler/jsonschematypes/ValidationError.java
+++ library/src/main/java/net/jimblackler/jsonschematypes/ValidationError.java
@@ -18,8 +18,9 @@ public class ValidationError {
   @Override
   public String toString() {
     URI schemaPath = schema.getUri();
-    String s = PathUtils.fetchFromPath(document, uri.getRawFragment()).toString();
-    return (s.length() <= 20 ? "\"" + s + "\" " : "")
+    Object object = PathUtils.fetchFromPath(document, uri.getRawFragment());
+    String string = object == null ? "" : object.toString();
+    return (string.length() <= 20 ? "\"" + string + "\" " : "")
         + (uri.toString().isEmpty() ? "" : "at " + uri + " ") + "failed "
         + (schemaPath.toString().isEmpty() ? "" : "against " + schemaPath + " ")
         + (schema instanceof BooleanSchema ? "" : "with \"" + message + "\"");
