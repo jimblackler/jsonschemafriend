0ff323b654df2a17957080296dba1325bdc8f069
Jim Blackler <jimblackler@google.com>
2020-09-02 15:24:15 +0100

    More complete test.

--- codegen/src/test/java/net/jimblackler/jsonschematypes/codegen/ExampleTest.java
+++ codegen/src/test/java/net/jimblackler/jsonschematypes/codegen/ExampleTest.java
@@ -9,19 +9,20 @@ public class ExampleTest {
   public static final FileSystem FILE_SYSTEM = FileSystems.getDefault();
 
   public static void main(String[] args) throws IOException {
-    Path base = FILE_SYSTEM.getPath("/examples");
-    //    scan(base.resolve("fstab"));
-    //    scan(base.resolve("standard").resolve("7-7-1-1"));
-    //    scan(base.resolve("misc"));
-    //    scan(base.resolve("longread"));
-    //    scan(base.resolve("meta"));
-    scan(base.resolve("docs"));
-  }
-
-  private static void scan(Path testDir) throws IOException {
     Path out = FILE_SYSTEM.getPath("out");
     FileUtils.createOrEmpty(out);
+
+    Path base = FILE_SYSTEM.getPath("/examples");
+    scan(out, "org.example.fstab", base.resolve("fstab"));
+    scan(out, "org.example.standard", base.resolve("standard").resolve("7-7-1-1"));
+    scan(out, "org.example.misc", base.resolve("misc"));
+    scan(out, "org.example.longread", base.resolve("longread"));
+    scan(out, "org.example.meta", base.resolve("meta"));
+    scan(out, "org.example.docs", base.resolve("docs"));
+  }
+
+  private static void scan(Path out, String namespace, Path testDir) throws IOException {
     new CodeGenerator(
-        out, "org.example", ExampleTest.class.getResource(testDir.resolve("schemas").toString()));
+        out, namespace, ExampleTest.class.getResource(testDir.resolve("schemas").toString()));
   }
 }
