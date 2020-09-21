13083793a6038df47862862af2580491ecb136eb
Jim Blackler <jimblackler@google.com>
2020-09-21 21:22:04 +0100

    Stop caching local files.

--- library/src/main/java/net/jimblackler/jsonschemafriend/CacheLoader.java
+++ library/src/main/java/net/jimblackler/jsonschemafriend/CacheLoader.java
@@ -9,23 +9,30 @@ import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.logging.Logger;
 
 public class CacheLoader {
+  private static final Logger LOG = Logger.getLogger(CacheLoader.class.getName());
   private static final FileSystem FILE_SYSTEM = FileSystems.getDefault();
 
   public static String load(URI uri) throws IOException {
-    Path diskCacheName = FILE_SYSTEM.getPath(System.getProperty("java.io.tmpdir"))
-                             .resolve("net.jimblackler.jsonschemafriend")
-                             .resolve("cache")
-                             .resolve(uri.getHost() + uri.getPath());
-    if (Files.exists(diskCacheName)) {
-      return streamToString(diskCacheName.toUri().toURL().openStream());
+    if ("http".equals(uri.getScheme()) || "https".equals(uri.getScheme())) {
+      Path diskCacheName = FILE_SYSTEM.getPath(System.getProperty("java.io.tmpdir"))
+                               .resolve("net.jimblackler.jsonschemafriend")
+                               .resolve("cache")
+                               .resolve(uri.getHost() + uri.getPath());
+      if (Files.exists(diskCacheName)) {
+        LOG.fine("Cache loading: " + uri + System.lineSeparator() + "From: " + diskCacheName);
+        return streamToString(diskCacheName.toUri().toURL().openStream());
+      }
+      String content = streamToString(uri.toURL().openStream());
+      diskCacheName.getParent().toFile().mkdirs();
+      try (PrintWriter out = new PrintWriter(diskCacheName.toFile())) {
+        out.println(content);
+      }
+      return content;
     }
-    String content = streamToString(uri.toURL().openStream());
-    diskCacheName.getParent().toFile().mkdirs();
-    try (PrintWriter out = new PrintWriter(diskCacheName.toFile())) {
-      out.println(content);
-    }
-    return content;
+    LOG.fine("Loading :" + uri);
+    return streamToString(uri.toURL().openStream());
   }
 }
