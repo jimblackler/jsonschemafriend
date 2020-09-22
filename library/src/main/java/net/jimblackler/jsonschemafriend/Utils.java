dd5790edffbe67b7fcdd3994db95e3ad24b2d9d6
Jim Blackler <jimblackler@google.com>
2020-09-22 10:57:06 +0100

    Class doesn't need to be public.

--- library/src/main/java/net/jimblackler/jsonschemafriend/Utils.java
+++ library/src/main/java/net/jimblackler/jsonschemafriend/Utils.java
@@ -3,7 +3,7 @@ package net.jimblackler.jsonschemafriend;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Utils {
+class Utils {
   static <T> Set<T> setOf(T element) {
     // Set.of() is only available in Java 9+. We try to keep the libary as compatible as possible.
     Set<T> set = new HashSet<>();
