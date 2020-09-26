e52e2bc9994d8fbde4828149465c6aa39a269f3b
Jim Blackler <jimblackler@google.com>
2020-09-26 21:53:04 +0100

    More methods supported in CombinedSchema.

--- library/src/main/java/net/jimblackler/jsonschemafriend/CombinedSchema.java
+++ library/src/main/java/net/jimblackler/jsonschemafriend/CombinedSchema.java
@@ -5,6 +5,7 @@ import static net.jimblackler.jsonschemafriend.TypeInferrer.inferTypes;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
 public class CombinedSchema {
@@ -57,5 +58,89 @@ public class CombinedSchema {
     }
 
     return allProperties;
+  }
+
+  public Collection<String> getNonProhibitedTypes() {
+    Collection<String> allTypes = new HashSet<>(TypeInferrer.getNonProhibitedTypes(schema));
+
+    Collection<Schema> anyOf = schema.getAnyOf();
+    if (anyOf != null && !anyOf.isEmpty()) {
+      // For the anyOf operator we return the intersection of the types of each subschema.
+      for (Schema subSchema : anyOf) {
+        allTypes.addAll(TypeInferrer.getNonProhibitedTypes(subSchema));
+      }
+    }
+
+    Collection<Schema> allOf = schema.getAllOf();
+    if (!allOf.isEmpty()) {
+      // For the anyOf operator we return the union of the types of each subschema.
+      for (Schema subSchema : allOf) {
+        Collection<String> types = TypeInferrer.getNonProhibitedTypes(subSchema);
+        allTypes.retainAll(types);
+      }
+    }
+
+    return allTypes;
+  }
+
+  public Collection<String> getExplicitTypes() {
+    Collection<String> allTypes = new HashSet<>();
+    Collection<String> explicitTypes = schema.getExplicitTypes();
+    if (explicitTypes != null) {
+      allTypes.addAll(explicitTypes);
+    }
+
+    Collection<Schema> anyOf = schema.getAnyOf();
+    if (anyOf != null && !anyOf.isEmpty()) {
+      // For the anyOf operator we return the intersection of the types of each subschema.
+      for (Schema subSchema : anyOf) {
+        allTypes.addAll(subSchema.getExplicitTypes());
+      }
+    }
+
+    Collection<Schema> allOf = schema.getAllOf();
+    if (!allOf.isEmpty()) {
+      // For the anyOf operator we return the union of the types of each subschema.
+      for (Schema subSchema : allOf) {
+        Collection<String> types = subSchema.getExplicitTypes();
+        allTypes.retainAll(types);
+      }
+    }
+
+    return allTypes;
+  }
+
+  public Collection<Object> getEnums() {
+    Collection<Object> allEnums = new HashSet<>();
+    List<Object> enums1 = schema.getEnums();
+    if (enums1 != null) {
+      allEnums.addAll(enums1);
+    }
+
+    Collection<Schema> anyOf = schema.getAnyOf();
+    if (anyOf != null && !anyOf.isEmpty()) {
+      // For the anyOf operator we return the intersection of the types of each subschema.
+      for (Schema subSchema : anyOf) {
+        List<Object> enums = subSchema.getEnums();
+        if (enums != null) {
+          allEnums.addAll(enums);
+        }
+      }
+    }
+
+    Collection<Schema> allOf = schema.getAllOf();
+    if (!allOf.isEmpty()) {
+      // For the anyOf operator we return the union of the types of each subschema.
+      for (Schema subSchema : allOf) {
+        Collection<Object> enums = subSchema.getEnums();
+        if (enums != null) {
+          allEnums.retainAll(enums);
+        }
+      }
+    }
+    if (allEnums.isEmpty()) {
+      return null;
+    }
+    return allEnums;
   }
 }
