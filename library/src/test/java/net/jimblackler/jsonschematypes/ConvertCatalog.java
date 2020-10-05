146fd9bbb7e6c71c6a97312552d83d340b301626
Jim Blackler <jimblackler@google.com>
2020-10-05 11:57:20 +0100

    New utility method (actually for the demo project).

---
+++ library/src/test/java/net/jimblackler/jsonschematypes/ConvertCatalog.java
@@ -0,0 +1,78 @@
+package net.jimblackler.jsonschematypes;
+
+import static net.jimblackler.jsonschemafriend.DocumentUtils.loadJson;
+import static net.jimblackler.jsonschematypes.ReaderUtils.getLines;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ConvertCatalog {
+  private static final FileSystem FILE_SYSTEM = FileSystems.getDefault();
+
+  public static void main(String[] args) throws IOException {
+    JSONObject out = new JSONObject();
+    JSONObject demos = new JSONObject();
+    out.put("demos", demos);
+    JSONObject schemas = new JSONObject();
+    out.put("schemas", schemas);
+
+    JSONArray allDemos = new JSONArray();
+
+    Path path0 = FILE_SYSTEM.getPath("/SchemaStore").resolve("src");
+    Path schemaPath = path0.resolve("schemas").resolve("json");
+    Path testDir = path0.resolve("test");
+    getLines(SuiteTest.class.getResourceAsStream(testDir.toString()), resource -> {
+      Path testSchema = schemaPath.resolve(resource + ".json");
+      InputStream resourceAsStream =
+          ConvertCatalog.class.getResourceAsStream(testSchema.toString());
+      if (resourceAsStream == null) {
+        System.out.println("Couldn't get " + testSchema);
+        return;
+      }
+      try {
+        schemas.put(resource, loadJson(resourceAsStream));
+      } catch (IOException e) {
+        e.printStackTrace();
+        return;
+      }
+
+      Path directoryPath = testDir.resolve(resource);
+      getLines(ConvertCatalog.class.getResourceAsStream(directoryPath.toString()), testFileName -> {
+        Path testFile = directoryPath.resolve(testFileName);
+        URL testDataUrl = ConvertCatalog.class.getResource(testFile.toString());
+        if (testDataUrl == null) {
+          return;
+        }
+
+        try {
+          JSONObject demo = new JSONObject();
+          demo.put("schema", resource);
+          demo.put("data", loadJson(ConvertCatalog.class.getResourceAsStream(testFile.toString())));
+          demos.put(testFileName, demo);
+          allDemos.put(testFileName);
+        } catch (JSONException | IOException e) {
+          e.printStackTrace();
+        }
+      });
+    });
+
+    try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(Paths.get("demos.json")))) {
+      writer.print(out.toString(2));
+    }
+
+    try (
+        PrintWriter writer = new PrintWriter(Files.newBufferedWriter(Paths.get("allDemos.json")))) {
+      writer.print(allDemos.toString(2));
+    }
+  }
+}
