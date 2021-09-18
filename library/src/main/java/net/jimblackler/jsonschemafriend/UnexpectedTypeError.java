cb7c6746d7335fb3b581bff80d7df25784efe6b5
Jim Blackler <jimblackler@google.com>
2021-09-18 17:03:01 +0100

    Remove redundant echoing of the URI in UnexpectedTypeError and clarify the message.

--- library/src/main/java/net/jimblackler/jsonschemafriend/UnexpectedTypeError.java
+++ library/src/main/java/net/jimblackler/jsonschemafriend/UnexpectedTypeError.java
@@ -4,19 +4,14 @@ import java.net.URI;
 
 public class UnexpectedTypeError extends ValidationError {
   private final Object object;
-  private final URI uri;
 
   public UnexpectedTypeError(URI uri, Object document, Object object, Schema schema) {
     super(uri, document, schema);
-    this.uri = uri;
     this.object = object;
   }
 
   @Override
   public String getMessage() {
-    if (uri.toString().isEmpty()) {
-      return "Unexpected type in data: " + object.getClass().getSimpleName();
-    }
-    return "Unexpected type in data at " + uri + " : " + object.getClass().getSimpleName();
+    return "Unexpected Java type in the document: " + object.getClass().getSimpleName();
   }
 }
