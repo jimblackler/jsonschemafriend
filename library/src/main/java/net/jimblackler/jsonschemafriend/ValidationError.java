4db3a7a2bf7c9499073331a44db25ae25104c85f
Jim Blackler <jimblackler@google.com>
2021-09-18 17:03:47 +0100

    Validation error shows JSON fragments to (hopefully) aid comprehension of errors.

--- library/src/main/java/net/jimblackler/jsonschemafriend/ValidationError.java
+++ library/src/main/java/net/jimblackler/jsonschemafriend/ValidationError.java
@@ -1,12 +1,17 @@
 package net.jimblackler.jsonschemafriend;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.net.URI;
 
 public abstract class ValidationError {
+  private static final String ELLIPSES = "...";
   private final URI uri;
   private final Object document;
   private final Schema schema;
   private final Object object;
+  private final ObjectMapper objectMapper = new ObjectMapper();
 
   protected ValidationError(URI uri, Object document, Schema schema) {
     this.uri = uri;
@@ -24,12 +29,15 @@ public abstract class ValidationError {
   @Override
   public String toString() {
     URI schemaPath = schema.getUri();
-
-    String string = object == null ? "" : object.toString();
-    return (string.length() <= 20 ? "\"" + string + "\" " : "")
-        + (uri.toString().isEmpty() ? "" : "at " + uri + " ") + "failed "
-        + (schemaPath.toString().isEmpty() ? "" : "against " + schemaPath + " ") + "with \""
-        + getMessage() + "\"";
+    try {
+      String str = objectMapper.writeValueAsString(object);
+      return (truncate(str, 60)
+          + (uri.toString().isEmpty() ? " at root " : " at " + uri + " ") + "failed "
+          + (schemaPath.toString().isEmpty() ? "" : "against " + schemaPath + " ") + "with \""
+          + getMessage() + "\"");
+    } catch (JsonProcessingException e) {
+      throw new IllegalStateException(e);
+    }
   }
 
   public Object getObject() {
@@ -49,4 +57,12 @@ public abstract class ValidationError {
   }
 
   public abstract String getMessage();
+
+  public static String truncate(String str, int maxLength) {
+    int trueMax = maxLength - ELLIPSES.length();
+    if (str.length() <= trueMax) {
+      return str;
+    }
+    return str.substring(0, trueMax) + ELLIPSES;
+  }
 }
