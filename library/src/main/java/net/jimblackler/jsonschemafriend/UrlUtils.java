61c19f2e5d31b10e7a05a7353f5949a962fb4b4f
Eli Sherer <eli.sherer@gmail.com>
2023-10-02 18:34:05 +0300

    mimic follow redirects of http to https in case content received empty

---
+++ library/src/main/java/net/jimblackler/jsonschemafriend/UrlUtils.java
@@ -0,0 +1,24 @@
+package net.jimblackler.jsonschemafriend;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import static net.jimblackler.jsonschemafriend.StreamUtils.streamToString;
+
+public class UrlUtils {
+  static String readFromStream(URL url) throws IOException {
+    String result;
+    try (InputStream stream = url.openStream()) {
+      result = streamToString(stream);
+    }
+    if (result.isEmpty() && "http".equals(url.getProtocol())) {
+      // in case tried http and received empty content, try to connect to same url with https
+      URL secureUrl = new URL(url.toString().replaceFirst("http", "https"));
+      try (InputStream stream = secureUrl.openStream()) {
+        result = streamToString(stream);
+      }
+    }
+    return result;
+  }
+}
