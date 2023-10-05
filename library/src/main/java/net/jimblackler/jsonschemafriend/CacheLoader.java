657a2a56eacdb79d6a9e3849b4b544cb9bdfb063
Jim Blackler <jimblackler@google.com>
2023-10-05 23:03:32 +0100

    Increment version and force caches to reset.

--- library/src/main/java/net/jimblackler/jsonschemafriend/CacheLoader.java
+++ library/src/main/java/net/jimblackler/jsonschemafriend/CacheLoader.java
@@ -17,7 +17,7 @@ public class CacheLoader implements Loader {
     if (cacheSchema && ("http".equals(uri.getScheme()) || "https".equals(uri.getScheme()))) {
       Path diskCacheName = FILE_SYSTEM.getPath(System.getProperty("java.io.tmpdir"))
                                .resolve("net.jimblackler.jsonschemafriend")
-                               .resolve("cache")
+                               .resolve("cache2")
                                .resolve(uri.getHost() + uri.getPath());
       if (Files.exists(diskCacheName)) {
         LOG.fine("Cache loading: " + uri + System.lineSeparator() + "From: " + diskCacheName);
