c163156bf2d62d6b24898374f132d332214cd83d
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2024-02-02 12:49:12 +0000

    Always try https

--- library/src/main/java/net/jimblackler/jsonschemafriend/UrlUtils.java
+++ library/src/main/java/net/jimblackler/jsonschemafriend/UrlUtils.java
@@ -1,7 +1,5 @@
 package net.jimblackler.jsonschemafriend;
 
-import com.sun.org.apache.xalan.internal.res.XSLTErrorResources;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -10,19 +8,21 @@ import static net.jimblackler.jsonschemafriend.StreamUtils.streamToString;
 
 public class UrlUtils {
   static String readFromStream(URL url) throws IOException {
+    IOException originalError = null;
     String result;
     try (InputStream stream = url.openStream()) {
       result = streamToString(stream);
     } catch (final IOException e) {
-      System.err.println(e.getMessage());
-      e.printStackTrace(System.err);
-      throw e;
+      originalError = e;
+      result = "";
     }
     if (result.isEmpty() && "http".equals(url.getProtocol())) {
       // in case tried http and received empty content, try to connect to same url with https
       URL secureUrl = new URL(url.toString().replaceFirst("http", "https"));
       try (InputStream stream = secureUrl.openStream()) {
         result = streamToString(stream);
+      } catch (final IOException e) {
+        throw originalError == null ? e : originalError;
       }
     }
     return result;
